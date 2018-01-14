4730c05375b5d382d92e37475a8eb9bf20f84248
Jamal O'Garro <jamal.ogarro[at]gmail.com>
2018-01-13 19:22:04 -0500

    add math_funcs utils

---
+++ utils/math_funcs.py
@@ -0,0 +1,19 @@
+"""Math Funcs
+
+Math utility functions to be used for function compositon
+"""
+def square(x):
+    """squares a givein number"""
+    return x ** 2
+
+def cube(x):
+    """cubes a given number"""
+    return x ** 3
+
+def less(x, y):
+    """Compares two numbers and returns True if x is less than y"""
+    return x < y
+
+def greater(x, y):
+    """Compares two numbers and retursn True if x is greater than y"""
+    return x > y
